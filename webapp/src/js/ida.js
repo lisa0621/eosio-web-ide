eaf3067ca080601e0a649e54d3197d3b7e4963f4
Sha-Pai LI <lisa0621@gmail.com>
2019-12-18 01:46:07 +0000

    add Dummy_Device_WebVer_for_IoTtalk

---
+++ webapp/src/js/ida.js
@@ -0,0 +1,26 @@
+ $(function(){
+        csmapi.set_endpoint ('https://5.iottalk.tw');
+        var profile = {
+		    'dm_name': 'Dummy_Device',          
+			'idf_list':[Dummy_Sensor],
+			'odf_list':[Dummy_Control],
+		    'd_name': undefined,
+        };
+		
+        function Dummy_Sensor(){
+            return Math.random();
+        }
+
+        function Dummy_Control(data){
+           $('.ODF_value')[0].innerText=data[0];
+        }
+      
+/*******************************************************************/                
+        function ida_init(){
+	    console.log(profile.d_name);
+	}
+        var ida = {
+            'ida_init': ida_init,
+        }; 
+        dai(profile,ida);     
+});
